f691560aed1eb7d31be8ed4096bbdd3c834e2db3
alfu-git <mdalfaz.dev@gmail.com>
2026-04-08 12:41:11 +0600

    move favicon logo assets to public folder

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { router } from "./Routes/Routes.jsx";
-import { RouterProvider } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import BookContextP from "./Context/BookContextP.jsx";
 import TabBtnContextP from "./Context/TabBtnContextP.jsx";
 import { ToastContainer } from "react-toastify";
